3b131013967ddb4b037fb1e5121d4d7a5b1e612a
Tyler Black <tfb8630@uncw.edu>
2024-02-20 13:51:36 -0500

    hw 2

--- InclassandMis/RecursivePrac.py
+++ InclassandMis/RecursivePrac.py
@@ -5,3 +5,19 @@ def reverse_string(string):
         return (string[len(string)-1]) + reverse_string(string[:len(string)-1])
 
 print(reverse_string("he"))
+
+
+def exp(a,n):
+    if n == 0:
+        return 1, 0
+    if n == 1:
+        return a, 0
+    if n % 2 == 0:
+        ans, count = exp(a * a, n // 2)
+        return ans, count + 1
+    else:
+        ans, count = exp(a * a, n // 2)
+        ans = a * ans
+        return ans, count + 2
+
+print(exp(3,3))
